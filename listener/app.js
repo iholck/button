55d3433809884e8312e1294a4171f99028fb1bfb
Ivar Holck <ivar.holck@gmail.com>
2017-12-07 21:20:33 +0100

    Get list of authorized macs from Service, check list before storing msg.

--- listener/app.js
+++ listener/app.js
@@ -7,6 +7,7 @@ var databaseTableName = "ButtonTable";
 var mqtt = require('mqtt');
 var tools = require('./tools');
 var crypto = require('crypto');
+var $ = require('jQuery');
 var mqttOptions = {
     keepalive: 10,
     clientId: "listener."+crypto.randomBytes(20).toString('hex'),
@@ -27,6 +28,8 @@ var mqttOptions = {
   };
 var mqttClient = mqtt.connect('mqtt://' + serverIP, mqttOptions);
 var sqlConnection = require('./db');
+var validMacs = [];
+
 
 function checkDatabase() {
 
@@ -66,6 +69,16 @@ function checkTableSetup() {
     });
 };
 
+function getValidMacs(){
+  var macs = [];
+  $.getJSON('http://10.10.12.72:3000/config/getConfigured',function(data) {
+    data.forEach( function (entry){
+          validMacs.push(entry.mac);
+      });
+    });
+    return macs;
+};
+
 function storeData(buttonName, buttonData) {
     var sqlQuery;
 
@@ -86,6 +99,8 @@ function storeData(buttonName, buttonData) {
 
 console.log('Doing DB checks..');
 checkTableSetup();
+console.log('Getting authorized mac addresses');
+validMacs = getValidMacs();
 
 console.log('Great! Going on to MQTT!');
 
@@ -99,8 +114,13 @@ mqttClient.on('connect', function() {
 mqttClient.on('message', function(topic, message) {
     // message is Buffer
     var parsMessage = message.toString().split(":");
+    // The format is macAddr:value.
     if (typeof(parsMessage[1]) !== "undefined" && parsMessage[1] !== null) {
+      if(validMacs.indexOf(parsMessage[0])!== -1){
         storeData(parsMessage[0], parsMessage[1]);
+      } else {
+        console.log('Invalid MAC Address');
+      }
     }
 
     //  console.log(message.toString());
